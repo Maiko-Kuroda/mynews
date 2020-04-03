e0860eb0d47df213ef82e025bf45b2f14cca2017
“Maiko-Kuroda” <maik0.kon@i.softbank.jp>
2020-04-03 14:56:52 +0000

    課題11

--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -15,22 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-});
-
-//課題１
-//└ Routing
-
-//課題２
-//└何度も同じ指示を書かなくていい
-//└プログラムのソースコードが分かりやすい。他の方がみても分かりやすい。
-
-//課題３
-//Route::get('xxx/','xxx\bbb')←❌
-//→Route::get('xxx','AAAcontroller@bbb');
-
-//課題４
-Route::group(['prefix'=>'admin'],function(){
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::get('profile/create','Admin\ProfileController@add');
     Route::get('profile/edit','Admin\ProfileController@edit');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
